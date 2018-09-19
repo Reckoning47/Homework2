449fd1cca422824abd4db16402caeea9a0d8d6b7
Clippy <ccdarthvader221@gmail.com>
2018-09-19 15:00:52 -0600

    2nd version

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+/*Chris Cooper
+*  ESOF 322 section 1
+*  9/19/2018
+*
+*  Sources: https://www.geeksforgeeks.org/merge-sort/
+*                                        /insertion-sort/
+*                                        /bubble-sort/
+*           "Head First Design Patterns"; Elisabeth Freeman and Kathy Sierra
+*
+*/
+
+
 public class Main {
     public static void main(String args[]){
         int myarray[] = {-1, 0, 9999, 1,3,2,7,5};
